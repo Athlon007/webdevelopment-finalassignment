e70019841dd87e2c8682d274cf6dc2c3d7623532
Athlon007 <aathlon@outlook.com>
2022-12-21 15:39:53 +0100

    Remove unused content

--- app/models/Exceptions/SessionFailException.php
+++ app/models/Exceptions/SessionFailException.php
@@ -1,13 +1,4 @@
 <?php
 class SessionFailException extends Exception
 {
-    public function __construct($message, $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
-    public function __toString()
-    {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
-    }
 }
